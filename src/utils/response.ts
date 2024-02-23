456521b53cb2615d4a516cd81c6a814c213a2884
Will G <sniper7kills@users.noreply.github.com>
2024-02-22 19:29:23 -0500

    fix: wip

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,9 +1,21 @@
 export function jsonResponseStringArrayConcat(object: JSONResponse): JSONResponse {
     for (const key in object) {
-        if (object[key] instanceof Array) {
+        if (key == 'raw') {
             object[key] = object[key].join('\n');
         } else if (object[key] instanceof Object) {
-            object[key] = JSON.stringify(jsonResponseStringArrayConcat(object[key]));
+            object[key] = jsonResponseStringArrayConcat(object[key]);
+        }
+    }
+    // Go though each key in the object.
+    // If the value is an array of strings, join them into a string.
+    // If the value is an object, call this function again.
+    for (const key in object) {
+        if (object[key] instanceof Array) {
+            if ((object[key] as Array<any>).every(item => typeof item === 'string')) {
+                object[key] = object[key].join('\n');
+            }
+        } else if (object[key] instanceof Object) {
+            object[key] = jsonResponseStringArrayConcat(object[key]);
         }
     }
     return object
