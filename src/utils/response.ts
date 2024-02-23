1cf48894e5e71a26ade4a6a5e4f3c9791538d83b
Will G <sniper7kills@users.noreply.github.com>
2024-02-22 19:21:15 -0500

    fix: stringify results

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -3,7 +3,7 @@ export function jsonResponseStringArrayConcat(object: JSONResponse): JSONRespons
         if (object[key] instanceof Array) {
             object[key] = object[key].join('\n');
         } else if (object[key] instanceof Object) {
-            object[key] = jsonResponseStringArrayConcat(object[key]);
+            object[key] = JSON.stringify(jsonResponseStringArrayConcat(object[key]));
         }
     }
     return object
